3960cd1ce0da51a3089964d1561b9e55fb0e01ae
Mohammed Natsha <natsha@google.com>
2021-09-07 01:53:49 +0200

    Connect and extract lineage from a database, skip query that doesn't contain lineage

--- google-datacatalog-mysql-connector/src/google/sql2parser/parseSql.py
+++ google-datacatalog-mysql-connector/src/google/sql2parser/parseSql.py
@@ -3,12 +3,33 @@ import os
 import json
 from types import SimpleNamespace
 from lineage import extractLineage
+import re
 
+noLineageQueryPatterns = [
+    re.compile("\\s*SET\\s+.*", re.IGNORECASE),
+    re.compile("\\s*DROP\\s+(.*)", re.IGNORECASE),
+    re.compile("\\s*EXPLAIN\\s+(.*)", re.IGNORECASE),
+    re.compile("\\s*SHOW\\s+(.*)", re.IGNORECASE),
+    re.compile("\\s*DESC\\s+(.*)", re.IGNORECASE),
+    re.compile("\\s*DESCRIBE\\s+(.*)", re.IGNORECASE),
+    re.compile("\\s*DESCRIBE\\s+(.*)", re.IGNORECASE),
+    re.compile("\\s*use\\s+(.*)", re.IGNORECASE)
+]
+
+def hasLineage(query):
+    global noLineageQueryPatterns
+
+    for pattern in noLineageQueryPatterns:
+        if pattern.match(query):
+            return False
+
+    # else it has lineage info
+    return True
 
 def parseQuery(query):
     path2jar = os.getcwd() + "/google-datacatalog-mysql-connector/extractLineage/target/test-1.0-SNAPSHOT-jar-with-dependencies.jar"
     x = subprocess.check_output(
-        ["java", "-jar", path2jar, "-p", query])  # doesn't capture output
+        ["java", "-jar", path2jar, "-p", query], stderr=subprocess.STDOUT)  # doesn't capture output
     # from byte to string
     x = x.decode('ascii')
 
