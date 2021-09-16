3a2969e0e65ad1453a9eff4d3f264cd716bed6ef
Mohammed Natsha <natsha@google.com>
2021-09-16 21:15:45 +0200

    extend parser to support Delete, insert, create table statements

--- google-datacatalog-mysql-connector/src/google/datacatalog_connectors/mysql_/lineage_synchronizer/scrape/parse/extractLineage/src/main/java/com/ParseSql.java
+++ google-datacatalog-mysql-connector/src/google/datacatalog_connectors/mysql_/lineage_synchronizer/scrape/parse/extractLineage/src/main/java/com/ParseSql.java
@@ -10,6 +10,8 @@ import org.apache.calcite.sql.parser.*;
 import org.apache.calcite.sql.parser.ddl.SqlDdlParserImpl;
 import org.apache.calcite.sql.validate.SqlConformanceEnum;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -52,10 +54,23 @@ public class ParseSql {
 
         return false;
     }
+
+    public static boolean isCreateOperation(String query) {
+        String updateRegex = "\\s*CREATE\\s+TABLE\\s+((?s).*)";
+        final Pattern pattern = Pattern.compile(updateRegex, Pattern.CASE_INSENSITIVE);
+        return pattern.matcher(query).matches();
+    }
+
     public static String preprocessQuery(String query) {
         // Replace " with '
         query = query.replace('\"', '\'');
         return query;
+    }
+
+    public static boolean IsDeleteFrom(String query) {
+        String deleteFromRegex = "DELETE\\s+(\\S+\\s+)FROM((?s).*)";
+        final Pattern pattern = Pattern.compile(deleteFromRegex, Pattern.CASE_INSENSITIVE);
+        return pattern.matcher(query).matches();
     }
 
     public static SqlNode parse(String query) throws SqlParseException {
@@ -68,9 +83,49 @@ public class ParseSql {
                     .setConformance(SqlConformanceEnum.MYSQL_5)
                     .setLex(Lex.MYSQL)
                     .build();
-            SqlParser par = SqlParser.create(query, sqlParserConfig);
-            return par.parseStmtList();
 
+            if(isCreateOperation(query)) {
+                int start = -1, end = -1;
+                int counter = 0;
+                for(int i=0;i < query.length();i++) {
+                    char c = query.charAt(i);
+                    if(c == '(') {
+                        start = start == -1? i: start;
+                        counter++;
+                    } else if(c == ')') {
+                        counter--;
+                        if(counter == 0) {
+                            end = i;
+                            break;
+                        }
+                    }
+                }
+
+                if(start != -1 && end != -1) {
+                    String columnDef = query.substring(start + 1, end);
+                    String[] columns = columnDef.split(",");
+
+                    String newColDef = "";
+                    for (String col: columns) {
+                        if(!col.trim().equals("") && !newColDef.equals("")) {
+                            newColDef += ",";
+                        }
+
+                        String arr[] = col.trim().split(" ", 2);
+
+                        if(!arr[0].equals(")")){
+                            newColDef += arr[0];
+                        }
+                    }
+
+                    query = query.replace(query.substring(start + 1, end), newColDef);
+                }
+
+            }
+
+            SqlParser par = SqlParser.create(query, sqlParserConfig);
+
+            return par.parseStmtList();
         }
 
         SqlParser.Config sqlParserConfig = SqlParser.configBuilder()
@@ -78,43 +133,62 @@ public class ParseSql {
                 .setLex(Lex.MYSQL)
                 .build();
 
+        if(IsDeleteFrom(query)) {
+            String deleteRegex = "DELETE\\s+(\\S+\\s+)?FROM(.*)";
+            final Pattern pattern = Pattern.compile(deleteRegex, Pattern.CASE_INSENSITIVE);
+            // find Matching patterns
+            Matcher matcher = pattern.matcher(query);
+            matcher.find();
+
+            String targetTable = matcher.group(1);
+            String subQuery = matcher.group(2);
+            String equivalentQuery = "INSERT INTO " + targetTable + " \n "
+                    + "SELECT * FROM " + subQuery;
+            SqlParser par = SqlParser.create(equivalentQuery, sqlParserConfig);
+            return par.parseStmtList();
+        }
+
         // Handling update is some special case
         // This Update works only with MySQL
-       if(isUpdateFromQuery(query)) {
-           System.out.println("it is update from");
-           String updateRegex = "update((?s).*)SET((?s).*)";
-           final Pattern pattern = Pattern.compile(updateRegex, Pattern.CASE_INSENSITIVE);
-           // find Matching patterns
-           Matcher matcher = pattern.matcher(query);
-           matcher.find();
-
-           String query1 = "SELECT * FROM FROM " + matcher.group(1);
-           String query2 = "UPDATE dummy_target SET " +  matcher.group(2);
-
-           SqlParser par1 = SqlParser.create(query1, sqlParserConfig);
-           SqlParser par2 = SqlParser.create(query2, sqlParserConfig);
-
-           SqlNode node1 = par1.parseStmtList();
-           SqlNode node2 = par2.parseStmtList();
-
-           ObjectMapper objectMapper = new ObjectMapper();
-           objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-           objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
-           objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-
-           try {
-               String t1 = objectMapper.writeValueAsString(node1);
-               String t2 = objectMapper.writeValueAsString(node2);
-
-               System.out.println(t1);
-               System.out.println(t2);
-           } catch (JsonProcessingException e) {
-               e.printStackTrace();
-           }
-
-           // TODO merge the 2 queries results
-           return node1;
-       }
+//       if(isUpdateFromQuery(query)) {
+//           System.out.println("it is update from");
+//           String updateRegex = "update((?s).*)SET((?s).*)";
+//           final Pattern pattern = Pattern.compile(updateRegex, Pattern.CASE_INSENSITIVE);
+//           // find Matching patterns
+//           Matcher matcher = pattern.matcher(query);
+//           matcher.find();
+//
+//           String query1 = "SELECT * FROM " + matcher.group(1);
+//           String query2 = "UPDATE dummy_target SET " +  matcher.group(2);
+//
+//           System.out.println(query1);
+//           System.out.println(query2);
+//           SqlParser par1 = SqlParser.create(query1, sqlParserConfig);
+//           SqlParser par2 = SqlParser.create(query2, sqlParserConfig);
+//
+//           SqlSelect node1 = (SqlSelect)par1.parseStmt();
+//           SqlUpdate node2 = (SqlUpdate)par2.parseStmt();
+//
+////           node2node1.getSelectList();
+//
+//           ObjectMapper objectMapper = new ObjectMapper();
+//           objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+//           objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+//           objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+//
+//           try {
+//               String t1 = objectMapper.writeValueAsString(node1);
+//               String t2 = objectMapper.writeValueAsString(node2);
+//
+//               System.out.println(t1);
+//               System.out.println(t2);
+//           } catch (JsonProcessingException e) {
+//               e.printStackTrace();
+//           }
+//
+//           // TODO merge the 2 queries results
+//           return node1;
+//       }
 
         SqlParser par = SqlParser.create(query, sqlParserConfig);
         return par.parseStmtList();
